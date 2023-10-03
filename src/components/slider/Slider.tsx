7b9c7cad79e1c36196811b227982cbe4d1d5a75f
shenalsj <shenal.sj@gmail.com>
2023-10-03 23:54:46 +0300

    First Layout

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img8 from "../../assets/images/img8.jpg";
+import img6 from "../../assets/images/img6.jpg";
+import "./Slider.scss";
+
+import { Carousel } from "react-responsive-carousel";
+
+
+
+const App: React.FC = () => {
+  return (
+    <div className="carasoul-style">
+      <div className="CarouselContainer">
+        <Carousel
+          showArrows={true}
+          showThumbs={false}
+          autoPlay={true}
+          infiniteLoop={true}
+          interval={3000}
+        >
+          <div className="slide">
+            <img src={img8} alt="Image 1" />
+            <p className="legend">Watches</p>
+          </div>
+          <div className="slide">
+            <img src={img6} alt="Image 2" />
+            <p className="legend">Mobile Phones</p>
+          </div>
+          <div className="slide">
+            <img src={img8} alt="Image 2" />
+            <p className="legend">Watches</p>
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default App;
